531ac77c036962d70101685477d9476c0a1a8187
Ana Vieira <aninhahouse12@gmail.com>
2023-03-14 11:52:38 -0300

    projeto Hello World

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int teste=10;
+		System.out.println("Helllo World!");
+        System.out.println("\nOlá Turma 62!");
+        System.out.println("Variável teste: " + teste);
+	}
+
+}
