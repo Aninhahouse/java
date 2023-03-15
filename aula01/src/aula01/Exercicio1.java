d4ca4c9c456797297b213a934ebbebd43875c0f1
Ana Vieira <aninhahouse12@gmail.com>
2023-03-15 14:02:05 -0300

    Exercicios da aula01

---
+++ aula01/src/aula01/Exercicio1.java
@@ -0,0 +1,25 @@
+package aula01;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+       Scanner entrada = new Scanner(System.in);
+		
+		float salario, abono, novoSalario;
+		
+		System.out.println("Digite seu salário: ");
+		salario = entrada.nextFloat();
+		System.out.println("Digite seu abono salarial: ");
+		abono = entrada.nextFloat();
+		
+		novoSalario = salario + abono;
+		
+		System.out.println("Seu novo salário é: " + novoSalario);
+		
+		entrada.close();
+
+	}
+
+}
